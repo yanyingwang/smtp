13e3ca5d4f4e887f26f82fbf8656d10a0418580c
Yanying Wang <yanyingwang1@gmail.com>
2020-02-06 21:12:49 +0800

    $ raco pkg new smtp-lib

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "smtp-lib")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/smtp-lib.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(yanying))
---
+++ scribblings/smtp-lib.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[smtp-lib
+                    racket/base]]
+
+@title{smtp-lib}
+@author{yanying}
+
+@defmodule[smtp-lib]
+
+Package Description Here
