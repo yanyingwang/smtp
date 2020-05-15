622edc094efd692fcb27b2480536d31caf02f58b
Yanying Wang <yanyingwang1@gmail.com>
2020-05-15 10:32:22 +0800

    rename to: smtp

--- info.rkt
+++ info.rkt
@@ -1,8 +1,8 @@
 #lang info
-(define collection "smtp-lib")
+(define collection "smtp")
 (define deps '("base" "gregor-lib" "at-exp-lib" "r6rs-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/smtp-lib.scrbl" ())))
-(define pkg-desc "send email with smtp protocol in Racket")
+(define scribblings '(("scribblings/smtp.scrbl" ())))
+(define pkg-desc "send email with smtp protocol")
 (define version "0.1")
 (define pkg-authors '("Yanying Wang"))
